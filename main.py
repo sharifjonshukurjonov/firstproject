083e09a66211ccfef072318d2a8f9d6153760b02
sharifjon <sharifjon.shukurjonov.2008.22.07@gmail.com>
2025-01-07 09:43:28 +0500

    chess check

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 def pawn_check(x1, y1, x2, y2):
+    if y1 == 2:
+        return x1 == x2 and y2 - y1 == 1 or y2 - y1 == 2
     return x1 == x2 and y2 - y1 == 1
 
-print(pawn_check(2, 3, 2, 4))
+# print(pawn_check(2, 3, 2, 4))
+
+def rook_check(x1, y1, x2, y2):
+    return x1 == x2 or y1 == y2
