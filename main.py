33a6f64946f1a24a9a75523c5f3c25610e14a08a
sharifjon <sharifjon.shukurjonov.2008.22.07@gmail.com>
2025-01-08 13:50:27 +0500

    changed

--- main.py
+++ main.py
@@ -19,6 +19,4 @@ def bishop_check(x1, y1, x2, y2):
 
 
 def knight_check(x1, y1, x2, y2):
-    ox = abs(x1 - x2)
-    oy = abs(y1 - y2)
-    return ox == 2 and oy == 1 or ox == 1 and oy == 2
+    return (x1 - x2) ** 2 + (y1 -y2) ** 2 ==5
