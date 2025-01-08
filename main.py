ac3bdcee3d9c03a22e2c35bd7809abb384f2a330
sharifjon <sharifjon.shukurjonov.2008.22.07@gmail.com>
2025-01-08 14:07:07 +0500

    Yangilandi

--- main.py
+++ main.py
@@ -19,4 +19,12 @@ def bishop_check(x1, y1, x2, y2):
 
 
 def knight_check(x1, y1, x2, y2):
-    return (x1 - x2) ** 2 + (y1 -y2) ** 2 ==5
+    return (x1 - x2) ** 2 + (y1 -y2) ** 2 == 5
+
+# print(rook_check(1, 1, 8, 2))
+
+
+def queen_check(x1, y1, x2, y2):
+    return abs(x1 - x2) == abs(y1 - y2) or x1 == x2 or y1 == y2
+
+# print(rook_check(1, 1, 8, 2))
