ce3fe8849dc0f29be5b0703edf7a6ced04dee21a
sharifjon <sharifjon.shukurjonov.2008.22.07@gmail.com>
2025-01-08 13:45:19 +0500

    shart qisqardi

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ def pawn_check(x1, y1, x2, y2):
 
 def rook_check(x1, y1, x2, y2):
     return x1 == x2 or y1 == y2
+
+# print(rook_check(1, 1, 8, 2))
+
+
+def bishop_check(x1, y1, x2, y2):
+    return abs(x1 - x2) == abs(y1 - y2)
+
+# print(rook_check(1, 1, 8, 2))
+
+
+
+def knight_check(x1, y1, x2, y2):
+    ox = abs(x1 - x2)
+    oy = abs(y1 - y2)
+    return ox == 2 and oy == 1 or ox == 1 and oy == 2
